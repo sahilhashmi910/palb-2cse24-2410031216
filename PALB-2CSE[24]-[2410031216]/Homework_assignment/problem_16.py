3ec61c3327962608a46b42dd083ef7809c99cdea
sahil hashmi <sahilhashmi910@gmail.com>
2026-02-12 23:48:28 +0530

    new file addded

--- PALB-2CSE[24]-[2410031216]/Homework_assignment/problem_16.py
+++ PALB-2CSE[24]-[2410031216]/Homework_assignment/problem_16.py
@@ -1,21 +1,20 @@
-def common_element(arr1,arr2,arr3):
-    i,j,k=0
-    n1,n2,n3=len(arr1),len(arr2),len(arr3)
-    result=[]
-    while i<n1 and j<n2 and k<3:
-        if arr1[i]==arr2[j]==arr3[k]:
-            result.append(arr1[i])
-            val=arr1[i]
-            while i<n1 and arr1[i]==val:i=i+1
-            while i<n1 and arr1[i]==val:j=j+1
-            while i<n1 and arr1[i]==val:k=k+1
-        elif arr1[i]<arr2[j]:
-            curr= arr1[i]
-            while j<n1 and arr1[i] ==curr:i=i+1
-        elif arr1[i]<arr2[j]:
-            curr= arr1[i]
-            while j<n1 and arr1[i] ==curr:i=i+1
-        elif arr1[i]<arr2[j]:
-            curr= arr1[i]
-            while j<n1 and arr1[i] ==curr:i=i+1
 
+
+def merge(intervals):
+    if not intervals:
+        return []
+
+    intervals.sort(key=lambda x: x[0])
+
+    merged = [intervals[0]]
+
+    for i in range(1, len(intervals)):
+        current_start, current_end = intervals[i]
+        last_merged_end = merged[-1][1]
+
+        if current_start <= last_merged_end:
+            merged[-1][1] = max(last_merged_end, current_end)
+        else:
+            merged.append(intervals[i])
+
+    return merged
